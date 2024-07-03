43e62f3584b30ff781c012b9a71a12d1f8b31b07
Liubov Poliakova <pluba5835@gmail.com>
2024-07-03 18:41:53 +0100

    please work

--- to-do-list/svelte.config.js
+++ to-do-list/svelte.config.js
@@ -1,12 +1,15 @@
 import adapter from '@sveltejs/adapter-static';
 
 export default {
-  kit: {
-    adapter: adapter({
-      pages: 'build',
-      assets: 'build',
-      fallback: '200.html',
-	  strict: false
-    })
-  }
+	kit: {
+		paths: {
+			base: dev ? '' : '/svelte-todo-list/tree/main/to-do-list' // Replace 'svelte-todo-list' with your repository name
+		},
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: '200.html',
+			strict: false
+		})
+	}
 };
