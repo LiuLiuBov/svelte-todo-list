f9403d04c5defb3e6524d395a6a4d0b940fbe2c6
Liubov Poliakova <pluba5835@gmail.com>
2024-07-03 17:36:38 +0100

    to do kist is ready

--- to-do-list/src/routes/+page.svelte
+++ to-do-list/src/routes/+page.svelte
@@ -1,2 +1,65 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<svelte:head>
+    <link rel="stylesheet" href="https://unpkg.com/@picocss/pico@latest/css/pico.min.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+</svelte:head>
+
+<script>
+    let toDoList = [];
+    let textInput = "";
+
+    function addToDo() {
+        toDoList = [...toDoList, { content: textInput, editing: false, checked: false }];
+        textInput = "";
+    }
+
+    function setEditing(i, isEditing) {
+        toDoList[i].editing = isEditing;
+    }
+
+    function deleteTodo(i) {
+        toDoList.splice(i, 1);
+        toDoList = toDoList;
+    }
+</script>
+
+<div class="container mt-5">
+    <div class="card">
+        <div class="card-body">
+            <h2 class="card-title text-center">ToDo List</h2>
+            <p>Enter your ToDo here</p>
+            <div class="input-group mb-3">
+                <input type="text" class="form-control" bind:value={textInput} placeholder="New ToDo">
+                <div class="input-group-append">
+                    <button class="btn btn-primary" on:click={addToDo}>Add</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    {#each toDoList as toDo, i}
+        <div class="card mt-3">
+            <div class="card-body d-flex align-items-center">
+                {#if toDo.editing}
+                    <input type="text" class="form-control mr-3" bind:value={toDo.content}>
+                {:else}
+                    <input type="checkbox" class="mr-3" bind:checked={toDo.checked}>
+                    <h4 class="flex-grow-1">{toDo.content}</h4>
+                {/if}
+                <div class="btn-group">
+                    {#if toDo.editing}
+                        <button class="btn btn-success" on:click={() => setEditing(i, false)}>Save</button>
+                    {:else}
+                        <button class="btn btn-secondary" on:click={() => setEditing(i, true)}>Edit</button>
+                    {/if}
+                    <button class="btn btn-danger" on:click={() => deleteTodo(i)}>Delete</button>
+                </div>
+            </div>
+        </div>
+    {/each}
+</div>
+
+<style>
+    .container {
+        max-width: 700px;
+    }
+</style>
